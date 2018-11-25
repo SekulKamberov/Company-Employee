686a237314c6c9d9045884387e48a9cb538d3dac
Секул Камберов <microfixbg@gmail.com>
2018-11-25 23:38:55 +0200

    Update Startup.cs

--- CompanyEmployee.API/Startup.cs
+++ CompanyEmployee.API/Startup.cs
@@ -38,7 +38,10 @@
             services.AddDomainServices();
             services.AddRouting(routing => routing.LowercaseUrls = true);
             services.AddAutoMapper();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options =>
+            {
+               options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
